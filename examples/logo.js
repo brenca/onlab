866fd6bf4e99e87ace351b53a45a15cb811751ae
Heilig Benedek <benecene@gmail.com>
2017-04-18 02:34:01 +0200

    implement logo #1

--- examples/logo.js
+++ examples/logo.js
@@ -11,13 +11,20 @@ class Logo extends Language {
     this.lexer.addTokenClasses([
       new Lexer.TokenClass('int', /[0-9]+(?![0-9]*\.[0-9]+)/),
       new Lexer.TokenClass('float', /[0-9]+\.[0-9]+/),
+      new Lexer.TokenClass('bool', /(true|false)/i),
+      new Lexer.TokenClass('not', /not/i),
+      new Lexer.TokenClass('and', /and/i),
+      new Lexer.TokenClass('or', /or/i),
       new Lexer.TokenClass('draw-cmd', 
-        /(fd|forward|bk|backward|rt|right|lt|left)/),
-      new Lexer.TokenClass('list-cmd', /(setpc|first|last|butfirst|butlast)/),
-      new Lexer.TokenClass('objlist-cmd', /(putfirst|putlast)/),
-      new Lexer.TokenClass('for-cmd', /for/),
-      new Lexer.TokenClass('word', new RegExp('"[a-z' + acc + '0-9]+')),
-      new Lexer.TokenClass('string', new RegExp('\[[a-z' + acc + ']+[a-z' + acc + '0-9 ]+\]')),
+        /(fd|forward|bk|backward|rt|right|lt|left)/i),
+      new Lexer.TokenClass('objlist-cmd', /(firstput|lastput|item)/i),
+      new Lexer.TokenClass('list-cmd', /(setpc|first|last|butfirst|butlast|uppercase|lowercase|count|empty\?)/i),
+      new Lexer.TokenClass('math-cmd', /(abs|sin|cos|tan|arcsin|arccos|arctan|exp|integer|int|log10|log)/i),
+      new Lexer.TokenClass('if-cmd', /if/i),
+      new Lexer.TokenClass('for-cmd', /for/i),
+      new Lexer.TokenClass('ctorlist-cmd', /list/i),
+      new Lexer.TokenClass('word', new RegExp('"[a-z' + acc + '0-9]*', 'i')),
+      new Lexer.TokenClass('literal', new RegExp('[a-z' + acc + '0-9]+', 'i')),
       new Lexer.TokenClass('char', /\S/)
     ])
     
@@ -25,24 +32,38 @@ class Logo extends Language {
       `<Program> ::= <Program> <Expression> | ""
       <SubProgram> ::= "[" <Program> "]"
       <List> ::= "[" <ListInner> "]"
-      <Expression> ::= <ListCommand> | <ObjectListCommand> | <DrawCommand> | <For>
-      <Parameter> ::= <Expression> | <Math> | <List> | "" | <Token-word> | <Token-string>
-      <ListCommand> ::= <Token-list-cmd> <ListOrString>
-      <ObjectListCommand> ::= <Token-objlist-cmd> <Parameter> <ListOrString>
-      <String> ::= <Token-word> | <Token-string>
+      <Expression> ::= <ListCommand> | <ObjectListCommand> | <MathCommand> | <DrawCommand> | <For> | <If> | <ConstructList>
+      <Parameter> ::= <Math> | <ListOrString>
+      <ListCommand> ::= <Token-list-cmd> <Parameter>
+      <ObjectListCommand> ::= <Token-objlist-cmd> <Parameter> <Parameter>
+      <MathCommand> ::= <Token-math-cmd> <Math>
       <ListOrString> ::= <String> | <List>
+      <String> ::= <Token-word> | <Token-literal>
       <DrawCommand> ::= <Token-draw-cmd> <Math>
       <For> ::= <Token-for-cmd> <Math> <SubProgram>
-      <ListInner> ::= <ListInner> <ListElement> | ""
-      <ListElement> ::= <Math> | <List> | <Expression>
+      <If> ::= <Token-if-cmd> <Bool> <SubProgram> <SubProgram>
+      <ListInner> ::= <ListInner> <Parameter> | ""
+      <ConstructList> ::= <Token-ctorlist-cmd> <TwoParameters> | "(" <Token-ctorlist-cmd> <Parameters> ")"
+      <TwoParameters> ::= <Parameter> <Parameter>
+      <Parameters> ::= <Parameter> <Parameters> | ""
       
       // Math expressions
-      <Math> ::= <InnerMath> | ""
-      <InnerMath> ::= <InnerMath> <PlusMinus> <MaybeMultiplyDivide> | <MaybeMultiplyDivide>
-      <SubMath> ::= "(" <Math> ")" | <Token-int> | <Token-float>
-      <MaybeMultiplyDivide> ::= <MaybeMultiplyDivide> <MultiplyDivide> <SubMath> | <SubMath>
-      <MultiplyDivide> ::= "*" | "/"
-      <PlusMinus> ::= "+" | "-"`
+      <Math> ::= <MathTerm> "+" <Math> | <MathTerm> "-" <Math> | <MathTerm>
+      <MathTerm> ::= <MaybeSignedMathFactor> "*" <MathTerm> | <MaybeSignedMathFactor> "/" <MathTerm> | <MaybeSignedMathFactor>
+      <MaybeSignedMathFactor> ::= "+" <MathFactor> | "-" <MathFactor> | <MathFactor>
+      <MathFactor> ::= <MathGroup> | <MathLiteral>
+      <MathGroup> ::= "(" <Math> ")"
+      <MathLiteral> ::= <Expression> | <Token-int> | <Token-float>
+      
+      // Boolean expressions
+      <Bool> ::= <BoolAND> <Token-or> <Bool> | <BoolAND>
+      <BoolAND> ::= <BoolEQ> <Token-and> <BoolAND> | <BoolEQ>
+      <BoolEQ> ::= <BoolCOMP> "==" <BoolEQ> | <BoolCOMP> "!=" <BoolEQ> | <BoolCOMP>
+      <BoolCOMP> ::= <BoolNOT> "<" <BoolCOMP> | <BoolNOT> ">" <BoolCOMP> | <BoolNOT> "<=" <BoolCOMP> | <BoolNOT> ">=" <BoolCOMP> | <BoolNOT>
+      <BoolNOT> ::= <Token-not> <BoolFactor> | <BoolFactor>
+      <BoolFactor> ::= "(" <Bool> ")" | <BoolLiteral>
+      <BoolLiteral> ::= <Math> | <Token-bool>
+      `
     )
     
     this._actions = {
@@ -58,6 +79,57 @@ class Logo extends Language {
           for (let i = 0; i < parameters[1]; i++)
             this.executeOne(parameters[2])
       },
+      'If': (node) => {
+        let parameters = node.children.map(child => {
+          if (child.rule.name !== 'SubProgram')
+            return this.executeOne(child)
+          else 
+            return child
+        })
+        
+        if (parameters.length === 4) {
+          if (parameters[1]) {
+            this.executeOne(parameters[2])
+          } else {
+            this.executeOne(parameters[3])
+          }
+        }
+      },
+      'ConstructList': (node) => {
+        let parameters = node.reducedChildren().map(child => {
+          return this.executeOne(child)
+        })
+        
+        console.log(parameters[0] ? parameters[0] : []);
+        
+        return parameters[0] ? parameters[0] : []
+      },
+      'TwoParameters': (node) => {
+        let list = []
+        
+        let parameters = node.children.forEach(child => {
+          if (child.rule.name !== 'Parameters') {
+            list.push(this.executeOne(child))
+          } else {
+            list = list.concat(this.executeOne(child))
+          }
+        })
+        
+        return list
+      },
+      'Parameters': (node) => {
+        let list = []
+        
+        let parameters = node.children.forEach(child => {
+          if (child.rule.name !== 'Parameters') {
+            list.push(this.executeOne(child))
+          } else {
+            list = list.concat(this.executeOne(child))
+          }
+        })
+        
+        return list
+      },
       'Program': (node) => {
         node.reducedChildren().map(child => {
           return this.executeOne(child)
@@ -68,21 +140,72 @@ class Logo extends Language {
           return this.executeOne(child)
         })
       },
+      'MathCommand': (node) => {
+        let parameters = node.children.map(child => {
+          return this.executeOne(child)
+        })
+        
+        let mathActions = {
+          'abs': (m) => {
+            return Math.abs(m)
+          },
+          'sin': (m) => {
+            return Math.sin(m)
+          },
+          'cos': (m) => {
+            return Math.cos(m)
+          },
+          'tan': (m) => {
+            return Math.tan(m)
+          },
+          'arcsin': (m) => {
+            return Math.asin(m)
+          },
+          'arccos': (m) => {
+            return Math.acos(m)
+          },
+          'arctan': (m) => {
+            return Math.atan(m)
+          },
+          'exp': (m) => {
+            return Math.exp(m)
+          },
+          'int': (m) => {
+            return Math.floor(m)
+          },
+          'log10': (m) => {
+            return Math.log10(m)
+          },
+          'log': (m) => {
+            return Math.log(m)
+          }
+        }
+        
+        mathActions['integer'] = mathActions['int']
+        // mathActions['backward'] = mathActions['bk']
+        // mathActions['right'] = mathActions['rt']
+        // mathActions['left'] = mathActions['lt']
+        
+        return mathActions[parameters[0].toLowerCase()](parameters[1])
+      },
       'ObjectListCommand': (node) => {
         let parameters = node.children.map(child => {
           return this.executeOne(child)
         })
         
         let objectListActions = {
-          'putfirst': (o, l) => {
+          'firstput': (o, l) => {
             return [o].concat(l)
           },
-          'putlast': (o, l) => {
+          'lastput': (o, l) => {
             return l.concat([o])
+          },
+          'item': (o, l) => {
+            return l[o - 1]
           }
         }
-        console.log(objectListActions[parameters[0]](parameters[1],parameters[2]));
-        return objectListActions[parameters[0]](parameters[1],parameters[2])
+        console.log(objectListActions[parameters[0].toLowerCase()](parameters[1],parameters[2]));
+        return objectListActions[parameters[0].toLowerCase()](parameters[1],parameters[2])
       },
       'ListCommand': (node) => {
         let parameters = node.children.map(child => {
@@ -104,30 +227,64 @@ class Logo extends Language {
           },
           'butlast': (l) => {
             return l.slice(0, -1)
+          },
+          'lowercase': (l) => {
+            if (typeof l === 'string' || l instanceof String) {
+              return l.toLowerCase()
+            } else {
+              return l.map(item => {
+                if (typeof item === 'string' || item instanceof String) {
+                  return item.toLowerCase()
+                } else {
+                  return item
+                }
+              })
+            }
+          },
+          'uppercase': (l) => {
+            if (typeof l === 'string' || l instanceof String) {
+              return l.toUpperCase()
+            } else {
+              return l.map(item => {
+                if (typeof item === 'string' || item instanceof String) {
+                  return item.toUpperCase()
+                } else {
+                  return item
+                }
+              })
+            }
+          },
+          'count': (l) => {
+            return l.length
+          },
+          'empty?': (l) => {
+            return l.length === 0
           }
         }
-        console.log(listActions[parameters[0]](parameters[1]));
-        return listActions[parameters[0]](parameters[1])
+        console.log(listActions[parameters[0].toLowerCase()](parameters[1]));
+        return listActions[parameters[0].toLowerCase()](parameters[1])
       },
       'List': (node) => {
         let parameters = node.reducedChildren().map(child => {
           return this.executeOne(child)
         })
         
-        return parameters[0]
+        return parameters[0] ? parameters[0] : []
       },
       'ListInner': (node) => {
         let list = []
+        
         let parameters = node.children.forEach(child => {
-          if (child.rule.name !== 'ListInner')
+          if (child.rule.name !== 'ListInner') {
             list.push(this.executeOne(child))
-          else
+          } else {
             list = list.concat(this.executeOne(child))
+          }
         })
         
         return list
       },
-      'InnerMath': (node) => {
+      'Math': (node) => {
         let parameters = node.children.map(child => {
           return this.executeOne(child)
         })
@@ -138,7 +295,31 @@ class Logo extends Language {
           },
           '-': (a, b) => {
             return a - b
-          },
+          }
+        }
+        
+        return mathActions[parameters[1]](parameters[0], parameters[2])
+      },
+      'MathFactor': (node) => {
+        let parameters = node.reducedChildren().map(child => {
+          return this.executeOne(child)
+        })
+        
+        return parameters[0]
+      },
+      'MathGroup': (node) => {
+        let parameters = node.reducedChildren().map(child => {
+          return this.executeOne(child)
+        })
+        
+        return parameters[0]
+      },
+      'MathTerm': (node) => {
+        let parameters = node.children.map(child => {
+          return this.executeOne(child)
+        })
+        
+        let mathActions = {
           '/': (a, b) => {
             return a / b
           },
@@ -148,6 +329,81 @@ class Logo extends Language {
         }
         
         return mathActions[parameters[1]](parameters[0], parameters[2])
+      },
+      'MaybeSignedMathFactor': (node) => {
+        let parameters = node.children.map(child => {
+          return this.executeOne(child)
+        })
+        
+        if (parameters.length === 1) {
+          return parameters[0]
+        } else {
+          if (parameters[0] === '-') {
+            return -parameters[1]
+          } else {
+            return parameters[1]
+          }
+        }
+      },
+      'Bool': (node) => {
+        let parameters = node.children.map(child => {
+          return this.executeOne(child)
+        })
+        
+        return parameters[0] || parameters[2]
+      },
+      'BoolAND': (node) => {
+        let parameters = node.children.map(child => {
+          return this.executeOne(child)
+        })
+        
+        return parameters[0] && parameters[2]
+      },
+      'BoolNOT': (node) => {
+        let parameters = node.children.map(child => {
+          return this.executeOne(child)
+        })
+        
+        return !parameters[1]
+      },
+      'BoolCOMP': (node) => {
+        let parameters = node.children.map(child => {
+          return this.executeOne(child)
+        })
+        
+        let boolActions = {
+          '<': (a, b) => {
+            return a < b
+          },
+          '>': (a, b) => {
+            return a > b
+          }
+        }
+        
+        return boolActions[parameters[1]](parameters[0], parameters[2])
+      },
+      'BoolEQ': (node) => {
+        let parameters = node.children.map(child => {
+          return this.executeOne(child)
+        })
+        
+        let boolActions = {
+          '==': (a, b) => {
+            return a == b
+          },
+          '!=': (a, b) => {
+            return a != b
+          }
+        }
+        
+        return boolActions[parameters[1]](parameters[0], parameters[2])
+      },
+      'BoolFactor': (node) => {
+        let parameters = node.reducedChildren().map(child => {
+          return this.executeOne(child)
+        })
+        
+        return parameters[0]
       },
       'DrawCommand': (node) => {
         let parameters = node.children.map(child => {
@@ -173,7 +429,7 @@ class Logo extends Language {
         drawActions['right'] = drawActions['rt']
         drawActions['left'] = drawActions['lt']
         
-        drawActions[parameters[0]](parameters[1])
+        drawActions[parameters[0].toLowerCase()](parameters[1])
       }
     }
     
@@ -187,8 +443,13 @@ class Logo extends Language {
       'word': (node) => {
         return node.rule.value.slice(1)
       },
-      'string': (node) => {
-        return node.rule.value.slice(1,-1)
+      'literal': (node) => {
+        return node.rule.value
+      },
+      'bool': (node) => {
+        if (node.rule.value === 'true')
+          return true
+        return false
       }
     }
   }
--- index.js
+++ index.js
@@ -14,4 +14,56 @@ let l = new Logo((c, a) => {
   console.log(c, a)
 })
 // l.execute(`first [1 2 3] for 5+5 [fd 100 rt 91.4 for 2 [right 1 lt 1] for 2 []]`)
-l.execute(`first [1 2 3] last [1 2 3] butfirst [1 2 3] butlast [1 2 3] putlast 1 [0] putfirst [0] [1] first "asd butlast [teszt elek]`)
+// l.execute(`fd 100 - (abs (-100 / 10)) first [first [abs -1] 2 3] last [1 2 3] butfirst [1 2 3] butlast [1 2 3] putlast 1 [0] putfirst [] [1] UpperCase "asd lowercase [ASD asd] butlast [teszt elek 99] for 2 [fd 100] if 10 != 9 [fd arcsin sin 1] [fd -10]`)
+l.execute(`empty? [] empty? " firstput int exp 1 [10] item 1 [1 2 3] first (list log10 15 2 3 4 5) fd 3`)
+
+
+/*
+heading
+hideturtle
+showturtle
+home
+list?
+make
+member?
+number?
+output
+pen
+pendown
+penup
+penwidth
+pi
+position
+power
+print
+random
+remainder -> %
+repeat -> for
+round
+run
+sentence
+setbg
+setheading
+setlinedash
+setpen
+setpencolor
+setpenwidth
+setposition
+setx
+sety
+shown?
+snap
+sqrt
+stop
+thing
+time
+date
+towards
+wait
+word
+word?
+xpos
+ypos
+
+
+*/
